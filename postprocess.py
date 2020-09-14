c290d4bd687fe5feeb89f8ea35e3028145954855
Elias Kunnas <elias@kunnas.com>
2020-09-14 11:56:25 +0300

    Update list of genres

--- postprocess.py
+++ postprocess.py
@@ -1,38 +1,16 @@
 #!/usr/bin/env python3
 import json
-import sys
-import glob
+import pandas as pd
 
 genres={}
-errors=[]
 
-for f in glob.glob('data/out_*.json'):
-    x = json.load(open(f))
+df_genres = pd.read_csv('genres.csv')
 
-    if 'genres' in x['value']:
-        for key, value in x['value']['genres'].items():
-            if key in ('size', '$size'):
-                continue
-
-            assert value['$size'] == 1, value
-            genres[key] = value['name']
-
-    for key, value in x['error'].items():
-        assert key == 'innerErrors', key
-        for e in value:
-            errors.append(e)
+for id_, name in zip(df_genres.id, df_genres.name):
+    genres[int(id_)] = name
 
 with open('genres.json', 'wt') as fh:
     json.dump(genres, fh, ensure_ascii=False, sort_keys=True, indent=2)
-with open('errors.json', 'wt') as fh:
-    json.dump(errors, fh, ensure_ascii=False, sort_keys=True, indent=2)
-
-with open('genres.csv', 'wt') as fh:
-    for id_, name in sorted(genres.items(), key=lambda x:int(x[0])):
-        print('{},{}'.format(id_, name), file=fh)
-with open('errors.csv', 'wt') as fh:
-    for e in errors:
-        print('{},{}'.format(e['pql'][1][0], e['message'] or ''), file=fh)
 
 body = '\n'.join(
     '<li><a href="https://www.netflix.com/browse/genre/{}">{}</a></li>'.format(id_, name)
